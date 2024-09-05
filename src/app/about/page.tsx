4d8a2a5d9bce9799597ee68e4926341480d8962e
Luigi Lupini <luigi.lupini@outlook.com>
2024-09-05 21:54:20 +0200

    Layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function AboutRoute() {
+  return (
+    <main className="p-5">
+      <h1 className="text-4xl font-bold">About Route</h1>
+      <p>This is the About Route</p>
+    </main>
+  );
+}
