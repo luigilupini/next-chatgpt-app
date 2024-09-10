8951df790ad17ad354b7309216dfe0b014a5f77a
Luigi Lupini <luigi.lupini@outlook.com>
2024-09-10 14:34:24 +0200

    Revert from JSON db test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import Todos from "@/components/todos";
-
 export default async function Home() {
   return (
     <main className="p-5">
       <h1 className="text-4xl font-bold">Home Route</h1>
       <p>This is the Home Route</p>
-      <Todos />
     </main>
   );
 }
